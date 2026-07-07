3841326899fd042edad944600e91ebfb6ed09315
rauf54409-del <rauf54409@gmail.com>
2026-07-07 06:21:44 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -21,9 +21,9 @@ const SHELL_ASSETS = [
   "./shared.js",
   "./parallax.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-maskable-512.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-maskable-512.png"
 ];
 
 // Hosts that must always be fetched live — never cached.
